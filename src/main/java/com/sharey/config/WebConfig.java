e937d3a01e47207fe85dc82c74779bdde6826408
dkqpeo <lan0184@naver.com>
2023-05-15 16:19:19 +0900

    docs:test

--- src/main/java/com/sharey/config/WebConfig.java
+++ src/main/java/com/sharey/config/WebConfig.java
@@ -7,5 +7,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
         // 추후 view resolver 설정
-    //test
+    //test dkdkdkdkdk
 }
